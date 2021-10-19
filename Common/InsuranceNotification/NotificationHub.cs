8c4890754c42f4005ff0192867ff057c20ebdd47
saud shahid <saudshahid_13@hotmail.com>
2021-10-20 01:59:04 +0500

    Added Notification to admin when new user is added

--- Common/InsuranceNotification/NotificationHub.cs
+++ Common/InsuranceNotification/NotificationHub.cs
@@ -4,10 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Common.InsuranceNotification
 {
     public class NotificationHub : Hub
     {
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+           
+        }
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+        }
+        public  void ReceiveNotifiction()
+        {
+
+            this.Clients.All.SendAsync("ReceiveNotifiction", "Your notification message");
+
+        }
     }
 }
--- InsuranceCompany/Controllers/SuperAdminController.cs
+++ InsuranceCompany/Controllers/SuperAdminController.cs
@@ -1,10 +1,17 @@
-﻿using Data;
+﻿using Common.InsuranceNotification;
+using Data;
+using Data.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 using Service.ServiceAction.HealthInsurance;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace InsuranceCompany.Controllers
 {
@@ -12,20 +19,72 @@ namespace InsuranceCompany.Controllers
     {
         private readonly IHealthInsuranceService _healthInsuranceService;
         private readonly InsuranceDBContext _insuranceDBContext;
-        public SuperAdminController(IHealthInsuranceService healthInsuranceService,InsuranceDBContext insuranceDBContext)
+        private readonly IConfiguration _configuration;
+        private static string conn = string.Empty;
+        private readonly IHubContext<NotificationHub> _hubContext;
+        public static int counteer = 0;
+        NotificationHub NotificationHub = new NotificationHub();
+        public SuperAdminController(IHealthInsuranceService healthInsuranceService, InsuranceDBContext insuranceDBContext, IConfiguration configuration, IHubContext<NotificationHub> hubContext)
         {
             _healthInsuranceService = healthInsuranceService;
             _insuranceDBContext = insuranceDBContext;
+            _configuration = configuration;
+            _hubContext = hubContext;
         }
         public IActionResult Index()
         {
+            
             return View();
         }
+        public IEnumerable<HealthInsurance> GetAllMessages()
+        {
+            conn = _configuration.GetConnectionString("db_string");
+            SqlDependency.Start(conn);
+            var messages = new List<HealthInsurance>();
+            using (var connection = new SqlConnection(conn))
+            {
+                connection.Open();
+                using (var command = new SqlCommand(@"SELECT [Id]
+                FROM [dbo].[HealthInsurances]", connection))
+                {
+                    command.Notification = null;
+
+                    var dependency = new SqlDependency(command);
+                    dependency.OnChange += new OnChangeEventHandler(dependency_OnChange);
+                    
+                    if (connection.State == ConnectionState.Closed)
+                        connection.Open();
+
+                    var reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        messages.Add(item: new HealthInsurance
+                        {
+                            Id = (long)reader["Id"]
+                        });
+                    }
+                }
+            }
+            return messages;
+        }
+
+        public void dependency_OnChange(object sender, SqlNotificationEventArgs e)
+        {
+            if (e.Type == SqlNotificationType.Change)
+            {
+                counteer+=1;
+                _hubContext.Clients.All.SendAsync("ReceiveNotifiction", counteer);
+            }
+            
+        }
+      
         [HttpGet]
         public IActionResult HealthListing()
         {
             try
             {
+
                 var draw = HttpContext.Request.Query["draw"];
 
                 // Skip number of Rows count  
@@ -81,6 +140,7 @@ namespace InsuranceCompany.Controllers
         }
         public IActionResult MedicareListing()
         {
+           
             return View();
         }
     }
